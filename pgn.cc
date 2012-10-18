d5f3e064e35992b075963a729126f26c0d862d31
Lucas Braesch <lucas.braesch@gmail.com>
2012-10-18 20:42:12 +0800

    PGN class

---
+++ pgn.cc
@@ -0,0 +1,50 @@
+/*
+ * Zinc, an UCI chess interface. Copyright (C) 2012 Lucas Braesch.
+ *
+ * Zinc is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Zinc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+*/
+#include "pgn.h"
+
+PGN::PGN(const Header& _hdeader)
+	: header(_hdeader)
+{}
+
+void PGN::operator<< (const std::string& san)
+{
+	san_list.push_back(san);
+}
+
+void PGN::Header::operator>> (std::ostream& ostrm) const
+{
+	ostrm << "[White \"" << white << "\"]\n";
+	ostrm << "[Black \"" << black << "\"]\n";
+
+	std::string result = (winner == White) ? "1-0" : (winner == Black ? "0-1" : "1/2-1/2");
+	ostrm << "[Result \"" << result << "\"]\n";
+
+	if (!fen.empty())
+		ostrm << "[FEN \"" << fen << "\"]\n";
+}
+
+void PGN::operator>> (std::ostream& ostrm) const
+/* FIXME: simplistic, need to implement things like "5.. Nf6 6. c4 (...) 35. Qxg7# 1-0" */
+{
+	header >> ostrm;
+	ostrm << std::endl;
+
+	for (auto it = san_list.begin(); it != san_list.end(); ++it)
+	{
+		ostrm << *it << ' ';
+	}
+
+	ostrm << std::endl;
+}
---
+++ pgn.h
@@ -0,0 +1,43 @@
+/*
+ * Zinc, an UCI chess interface. Copyright (C) 2012 Lucas Braesch.
+ *
+ * Zinc is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Zinc is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include <vector>
+#include <string>
+#include <ostream>
+#include "engine.h"
+#include "types.h"
+
+class PGN
+{
+public:
+	struct Header
+	{
+		std::string white, black;
+		Color winner;
+		std::string fen;
+		Engine::SearchParam sp;
+
+		void operator>> (std::ostream& ostrm) const;
+	};
+
+	PGN(const Header& _header);
+
+	void operator<< (const std::string& san);
+	void operator>> (std::ostream& ostrm) const;
+
+private:
+	Header header;
+	std::vector<std::string> san_list;
+};
