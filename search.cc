321fbae21b1618ae6f5fd1eeae10b067fa660970
Lucas Braesch <lucas.braesch@gmail.com>
2012-11-26 18:44:42 +0800

    more QS futility pruning
    cutechess-cli -each proto=uci tc=5+0.05 book=./Engines/Book.bin bookdepth=8 option.Hash=32 \
    	-engine cmd=./chess/chess name=test -engine cmd=./chess/bench name=bench \
    	-games 10000 -concurrency 2 -repeat \
    	-draw movenumber=120 movecount=5 score=100 \
    	-resign movecount=3 score=700 \
    	-sprt elo0=0 elo1=5 alpha=0.05 beta=0.10
    
    Test aborted at: 1190-1093-1411 [51.3%]. early termination so LOS is meaningless.
    The log likelyhood is ll = 1.89 with an upper bound b = 2.89, quite a long way to go,
    I decided to be an heretic, and accept H1 early, hoping that further sampling would eventually bring up the
    log-likelyhood to the upper bound. And it probably woud, because my testing is ultimately cyclical: hash tables
    are flushed upon ucinewgame, and there are only a finite number of book lines <= 8 moves...
    
    In short: patch accepted in a leap of faith :)
    
    signature = 5687063

--- search.cc
+++ search.cc
@@ -331,8 +331,8 @@ namespace
 					continue;
 				}
 
-				// the "SEE proxy" tells us we are unlikely to raise alpha, skip if we are deep into the qsearch
-				if (fut_base <= alpha && depth <= QS_LIMIT/2 && see <= 0) {
+				// the "SEE proxy" tells us we are unlikely to raise alpha, skip if depth < 0
+				if (fut_base <= alpha && depth < 0 && see <= 0) {
 					best_score = std::max(best_score, fut_base);	// beware of fail soft side effect
 					continue;					
 				}
