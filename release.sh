0cc195590b085bd3ed90e59edbdd3e53efcf913a
Lucas Braesch <lucas.braesch@gmail.com>
2013-05-28 15:39:23 +0800

    release script added to avoid mistakes when doing various compiles.

---
+++ release.sh
@@ -0,0 +1,7 @@
+# Linux (dynamic compiles)
+g++ ./src/*.cc -o ./bin/${1}_sse2 -DNDEBUG -std=c++11 -pedantic -O3 -msse2 -fno-rtti -flto -s
+g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=c++11 -pedantic -O3 -msse4.2 -fno-rtti -flto -s
+
+# Windows (static mingw compiles)
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe -DNDEBUG -std=c++0x -pedantic -O3 -msse2 -fno-rtti -flto -s -static
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=c++0x -pedantic -O3 -msse4.2 -fno-rtti -flto -s -static
