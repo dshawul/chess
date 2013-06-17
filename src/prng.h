59ce0a5ec6bd1537661c444ac29399ac7e9ca211
Lucas Braesch <lucas.braesch@gmail.com>
2013-06-17 18:54:32 +0800

    seedable PRNG signature = 6572790

--- src/prng.h
+++ src/prng.h
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  *
- * Credits: Bob Jenkins' 64-bit PRNG from the Marsaglia "KISS family". Seeds from Heinz Van Saanen.
+ * Credits: Bob Jenkins' 64-bit KISS PRNG and seeding algorithm.
 */
 #pragma once
 #include <cinttypes>
@@ -22,12 +22,17 @@ struct PRNG {
 
 	// Return 64 bit unsigned integer in between [0, 2^64 - 1]
 	uint64_t rand() {
-		const uint64_t
-		e = a - rotate(b,  7);
+		const uint64_t e = a - rotate(b,  7);
 		a = b ^ rotate(c, 13);
 		b = c + rotate(d, 37);
 		c = d + e;
 		return d = e + a;
+	}
+	
+	void init(uint64_t seed = 0) {
+		a = 0xf1ea5eed, b = c = d = seed;
+		for (int i = 0; i < 20; ++i)
+			rand();
 	}
 
 private:
@@ -36,13 +41,5 @@ private:
 
 	uint64_t rotate(uint64_t x, uint64_t k) const {
 		return (x << k) | (x >> (64 - k));
-	}
-
-	// Init seed and scramble a few rounds
-	void init() {
-		a = 0x46dd577ff603b540;
-		b = 0xc4077bddfacf987b;
-		c = 0xbbf4d93b7200e858;
-		d = 0xd3e075cfd449bb1e;
 	}
 };
