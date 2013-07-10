d6f6380d8b8ee81b2b6a85101ff9bf6b36bc6f3f
Lucas Braesch <lucas.braesch@gmail.com>
2013-07-10 18:31:39 +0800

    skip IID when in check (6667715)

--- src/search.cc
+++ src/search.cc
@@ -261,7 +261,8 @@ int search(board::Position& B, int alpha, int beta, int depth, int node_type, Se
 
 	// Internal Iterative Deepening
 	if ( (!tte || !tte->move || tte->depth <= 0)
-		 && depth >= (node_type == PV ? 4 : 7) ) {
+		 && depth >= (node_type == PV ? 4 : 7)
+		 && !in_check ) {
 		ss->skip_null = true;
 		search(B, alpha, beta, node_type == PV ? depth - 2 : depth / 2, node_type, ss);
 		ss->skip_null = false;
