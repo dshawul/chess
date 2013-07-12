fad0a25a1f980933bcedccb6a74c690977bdd73a
Lucas Braesch <lucas.braesch@gmail.com>
2013-07-12 19:45:01 +0800

    use automatic YYYYMMDD version numbers I've had enough of forgetting to manually change the version number before releasing! Now use automatic YYYYMMDD version given by the last git commit. So the date is based on the actual code rather than the time of compilation.

--- release.sh
+++ release.sh
@@ -1,11 +1,22 @@
-# Linux (dynamic compiles)
-g++ ./src/*.cc -o ./bin/${1}_sse2 -DNDEBUG -std=c++11 -pedantic -O3 -msse2 -fno-rtti -flto -s
-g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=c++11 -pedantic -O3 -msse4.2 -fno-rtti -flto -s
+# Find the date of the last commit, in the format: YYYYMMDD. We use this date as automatic version
+# number, passed through the preprocessor variable DISCO_VERSION
+vd=`git log -1 --format="%ci"|sed "s/^\([0-9]*\)\-\([0-9]*\)\-\([0-9]*\)\>.*/\1\2\3/g"`
+name="discocheck_"${vd}
+echo "version number: ${vd}"
 
-# Windows (static mingw compiles)
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe -DNDEBUG -std=c++0x -pedantic -O3 -msse2 -fno-rtti -flto -s -static
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=c++0x -pedantic -O3 -msse4.2 -fno-rtti -flto -s -static
+echo "building linux compiles"
+g++ ./src/*.cc -o ./bin/${name}_sse2 -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++11 -pedantic \
+	-O3 -msse2 -fno-rtti -flto -s
+g++ ./src/*.cc -o ./bin/${name}_sse4.2 -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++11 -pedantic \
+	-O3 -msse4.2 -fno-rtti -flto -s
 
-# Make compressed tarball
-tar czf ./bin/${1}.tar.gz ./bin/${1}_sse*
-rm ./bin/${1}_sse*
+echo "building windows compiles"
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${name}_sse2.exe -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++0x -pedantic \
+	-O3 -msse2 -fno-rtti -flto -s -static
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${name}_sse4.2.exe -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++0x -pedantic \
+	-O3 -msse4.2 -fno-rtti -flto -s -static
+
+echo "make tarball and cleanup"
+tar czf ./bin/${name}.tar.gz ./bin/${name}_sse*
+rm ./bin/${name}_sse*
+
