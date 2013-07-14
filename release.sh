f4a0ace18ff718f955ca4bf70e4a5125c035f51c
Lucas Braesch <lucas.braesch@gmail.com>
2013-07-14 09:55:06 +0800

    use YYYYMMDD only in make.sh not in release.sh

--- release.sh
+++ release.sh
@@ -1,22 +1,21 @@
-# Find the date of the last commit, in the format: YYYYMMDD. We use this date as automatic version
-# number, passed through the preprocessor variable DISCO_VERSION
-vd=`git log -1 --format="%ci"|sed "s/^\([0-9]*\)\-\([0-9]*\)\-\([0-9]*\)\>.*/\1\2\3/g"`
-name="discocheck_"${vd}
-echo "version number: ${vd}"
+name="discocheck_"$1
+echo ${name}
 
 echo "building linux compiles"
-g++ ./src/*.cc -o ./bin/${name}_sse2 -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++11 -pedantic \
+g++ ./src/*.cc -o ./bin/${name}_sse2 -DNDEBUG -DDISCO_VERSION=\"${1}\" -std=c++11 -pedantic \
 	-O3 -msse2 -fno-rtti -flto -s
-g++ ./src/*.cc -o ./bin/${name}_sse4.2 -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++11 -pedantic \
+g++ ./src/*.cc -o ./bin/${name}_sse4.2 -DNDEBUG -DDISCO_VERSION=\"${1}\" -std=c++11 -pedantic \
 	-O3 -msse4.2 -fno-rtti -flto -s
 
 echo "building windows compiles"
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${name}_sse2.exe -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++0x -pedantic \
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${name}_sse2.exe -DNDEBUG -DDISCO_VERSION=\"${1}\" -std=c++0x -pedantic \
 	-O3 -msse2 -fno-rtti -flto -s -static
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${name}_sse4.2.exe -DNDEBUG -DDISCO_VERSION=\"${vd}\" -std=c++0x -pedantic \
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${name}_sse4.2.exe -DNDEBUG -DDISCO_VERSION=\"${1}\" -std=c++0x -pedantic \
 	-O3 -msse4.2 -fno-rtti -flto -s -static
 
 echo "make tarball and cleanup"
-tar czf ./bin/${name}.tar.gz ./bin/${name}_sse*
-rm ./bin/${name}_sse*
+cd ./bin
+tar czf ./${name}.tar.gz ./${name}_sse*
+rm ./${name}_sse*
+cd ..
 
