2ad97e543b3d29d6d35d92e70de1318a0a621f57
Lucas Braesch <lucas.braesch@gmail.com>
2013-07-18 18:35:48 +0800

    restore -DNDEBUG

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 astyle ./src/* -A4T4LYm0poH
 rm ./src/*.orig
 
-g++ ./src/*.cc -o $1 -std=c++11 -Wall -pedantic \
+g++ ./src/*.cc -o $1 -std=c++11 -Wall -pedantic -DNDEBUG \
 	-O3 -msse4.2 -fno-rtti -flto -s
