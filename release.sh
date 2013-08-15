7cee963a0f5f2ca1bcb9ee2de9aee3d7d6119926
Lucas Braesch <lucas.braesch@gmail.com>
2013-08-15 22:13:48 +0800

    fix release.sh for VLA

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 echo "building linux compiles"
-g++ ./src/*.cc -o ./bin/${1}_sse2 -DNDEBUG -std=c++11 -Wall -pedantic \
+g++ ./src/*.cc -o ./bin/${1}_sse2 -DNDEBUG -std=gnu++11 -Wall \
 	-O3 -msse2 -fno-rtti -flto -s
-g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=c++11 -Wall -pedantic \
+g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=gnu++11 -Wall \
 	-O3 -msse4.2 -fno-rtti -flto -s
 
 echo "building windows compiles"
-# Note: currently mingw uses an old GCC, which only supports -std=c++0x, and -flto is *broken*
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe -DNDEBUG -std=c++0x -Wall -pedantic \
+# Note: currently mingw uses an old GCC, which only supports -std=gnu++0x, and -flto is *broken*
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe -DNDEBUG -std=gnu++0x -Wall \
 	-O3 -msse2 -fno-rtti -s -static
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=c++0x -Wall -pedantic \
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=gnu++0x -Wall \
 	-O3 -msse4.2 -fno-rtti -s -static
 
 echo "make tarball and cleanup"
