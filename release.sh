8b0cc43512a6c70d7fff7fc5edb9369e99d6b178
Lucas Braesch <lucas.braesch@gmail.com>
2013-10-28 19:38:55 +0800

    use flto with mingw

--- release.sh
+++ release.sh
@@ -1,15 +1,10 @@
 echo "building linux compiles"
-g++ ./src/*.cc -o ./bin/${1}_sse2 -DNDEBUG -std=gnu++11 -Wall \
-	-O3 -msse2 -fno-rtti -flto -s
-g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=gnu++11 -Wall \
-	-O3 -msse4.2 -fno-rtti -flto -s
+g++ ./src/*.cc -o ./bin/${1}_sse2 -DNDEBUG -std=gnu++11 -Wall -O3 -msse2 -fno-rtti -flto -s
+g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=gnu++11 -Wall -O3 -msse4.2 -fno-rtti -flto -s
 
 echo "building windows compiles"
-# Note: currently mingw uses an old GCC, which only supports -std=gnu++0x, and -flto is *broken*
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe -DNDEBUG -std=gnu++0x -Wall \
-	-O3 -msse2 -fno-rtti -s -static
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=gnu++0x -Wall \
-	-O3 -msse4.2 -fno-rtti -s -static
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe -DNDEBUG -std=gnu++0x -Wall -O3 -msse2 -fno-rtti -s -static -flto
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=gnu++0x -Wall -O3 -msse4.2 -fno-rtti -s -static -flto
 
 echo "make tarball and cleanup"
 cd ./bin
